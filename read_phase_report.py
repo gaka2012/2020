adfc294be6abeb60ca44f7705d21d8bbc768ce84
zhangzhipeng <1224881267@qq.com>
2020-01-02 16:13:02 +0800

    temporary

--- read_phase_report.py
+++ read_phase_report.py
@@ -10,19 +10,17 @@ from obspy.core import UTCDateTime
 import matplotlib.pyplot as plt
 import numpy as np
 
-st=['WDT','ZHQ','JZG','QCH','PWU','JMG','ZJG','AXI','WCH','CD2','YZP','BAX','MDS','TQU','GZA','HMS']
+sta_list=['WDT','ZHQ','JZG','QCH','PWU','JMG','ZJG','AXI','WCH','CD2','YZP','BAX','MDS','TQU','GZA','HMS']
 #st          = ['HWS','ANZ']         #需要寻找的台站
-phase       = ['Pg','Sg','Pn','Sn','P','S'] #需要寻找的震相
-report_path = 'report-file-2018-2019'       #震相报告所在路径
-out_path    = 'dist-time'                   #将结果文件移动到此路径下
+phase_list  = ['Pg','Sg','Pn','Sn','P','S'] #需要寻找的震相
+report_path = 'report'       #震相报告所在路径
+out_name    = 'result.txt'                   #将结果文件移动到此路径下
 lat_min     = 29
 lat_max     = 34
 lon_min     = 101
 lon_max     = 107
 begin_time  = UTCDateTime('2017-01-01 00:00:00')
 end_time    = UTCDateTime('2019-12-31 00:00:00')
-plot_travel_time=True                      #是否画图
-plot_path   = 'picture'                    #画图之后移动到此位置。
 
 
 earth_num=0 #震相报告中的地震数量
@@ -45,89 +43,28 @@ for report_file in report_files:
            try:
                if part[0]=='DBO' : #DBO表示是某个地震
                    bjtime  = UTCDateTime(part[2]+' '+part[3]) #发震时刻
-                   if lat_min<=float(part[4])<=lat_max and  lon_min<=float(part[5])<=lon_max and  begin_time<=bjtime<=end_time:
+                   magnitude = float(line[56:62])             #震级
+                   if lat_min<=float(part[4])<=lat_max and  lon_min<=float(part[5])<=lon_max and  begin_time<=bjtime<=end_time and mag_min<=magnitude<=mag_max:
                        earth=True #符合条件才会继续寻找相应的拾取台站到时信息
                        gmttime = bjtime-8*3600
                        earth_num+=1
+                       fa=open(out_name,'a+')
+                       fa.write(line)
+                       fa.close()
                    else :
                        earth=False
                if earth:
-                   if part[2] in st and (line[24:28].split()[0] in phase):
-                       try:
-                           arrival_time = UTCDateTime(line[34:56])
-                           travle_time  = arrival_time-bjtime #走时
-                           dist         = float(line[65:71])  #震中距
-                           out_name     = part[2]+'-'+line[24:28].split()[0]+'.dat'
-                           fa=open(out_name,'a+')
-                           fa.write(str(dist)+' '+str(travle_time)+'\n')
-                           fa.close()
-                           phase_num+=1
-                       except Exception as e: #可能会遇到没有震中距的情况，dist=float就会报错。所以用了except
-                           #print (report_file,line_num) #可以显示报错所在的文件及行数。
-                           continue
+                   if part[2] in sta_list and (line[24:28].split()[0] in phase_list):
+                       fa=open(out_name,'a+')
+                       fa.write(line)
+                       fa.close()
            except IndexError:
                continue
-os.system('mv *.dat %s'%(out_path))    
+   
 print ('total earthquake ==',earth_num)
 print ('total phase      ==',phase_num)
 
 
-#画走时表
-exist_phase=0 #统计存在多少震相
-if plot_travel_time:
-    for sta in st:
-        #每个台站画一幅图,一幅图包括多个震相的散点图及拟合的曲线。
-        max_dist   =0 #最大dist,用来作为x轴最大值
-        max_travel =0 #最大走时，用来作为y轴最大值
-        try:
-            plt.figure(figsize=(25,15)) 
-            for ph in phase:
-                tem_dist   = []
-                tem_travel = []
-                #由于部分台站可能没有人工拾取，所以首先要判断是否存在。
-                name        = sta+'-'+ph+'.dat'
-                result_path = os.path.join(out_path,name)
-                if os.path.exists(result_path):
-                    exist_phase+=1
-                    with open(result_path,'r') as fr:
-                        for line in fr:
-                            part=line.split()
-                            tem_dist.append(float(part[0]))
-                            tem_travel.append(float(part[1]))
-                    #根据dist和time画图，标签是震相名称
-                    if ph=='Pg':
-                        color='r'
-                    elif ph=='Pn':
-                        color='k'
-                    elif ph=='Sg':
-                        color='b'
-                    elif ph=='Sn':
-                        color='g'
-                    plt.scatter(tem_dist,tem_travel,s=10,c=color,label=ph)
-                    tem_max_dist   = max(tem_dist)
-                    tem_max_travel = max(tem_travel)
-                    #print (max_dist)
-                    if tem_max_dist>max_dist:
-                        max_dist=tem_max_dist
-                    if tem_max_travel>max_travel:
-                        max_travel=tem_max_travel
-                    tem_dist,tem_travel=[],[]            
-                else:
-                    pass
-            #print (max_dist)
-            plt.xticks(np.arange(0,max_dist+50,10))   
-            plt.yticks(np.arange(0,max_travel+10,10)) 
-            plt.legend()
-            plt.xlabel('dist',fontsize=25)
-            plt.ylabel('travel-time',fontsize=25)
-            plt.title(sta,fontsize=25)
-            plt.savefig(sta)
-            plt.close()
-        except Exception as e:
-            print (e)
-        #print (sta,len(dist),len(dist[0])) #每个台站都有2个列表，一个存储dist,一个存储time.并且是嵌套的，嵌套不同的震相。
-    print ('exist_phase ==',exist_phase)
-    os.system('mv *.png %s'%(plot_path))
 
 
 
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
-
+'''
+使用方法 python test.py -t 2019-09-
+必须有的选项 -t -lat -lon 
+可选选项     -m -sta -pha
+'''
 
 import sys
 
 in_put   = sys.argv
 
+mag_index   = False
+sta_index   = False
+phase_index = False
 for content in in_put:
     if content == '-t':
         start_time_index = in_put.index(content)
@@ -17,13 +24,42 @@ for content in in_put:
         lon_index = in_put.index(content)
     elif content == '-sta':
         sta_index = in_put.index(content)
+    elif content == '-pha':
+        phase_index = in_put.index(content)
 start_time = in_put[start_time_index+1]
 end_time   = in_put[start_time_index+2]
-lat_left   = in_put[lat_index+1]
-lat_right  = in_put[lat_index+2]
-lon_left   = in_put[lon_index+1]
-lon_right  = in_put[lon_index+2]
+begin_time  = UTCDateTime(start_time)
+end_time    = UTCDateTime(end_time)
+lat_min   = in_put[lat_index+1]
+lat_max  = in_put[lat_index+2]
+lon_min   = in_put[lon_index+1]
+lon_max  = in_put[lon_index+2]
 
-mag        = in_put[mag_index+1]
-sta        = in_put[sta_index+1]
-print (sta)
+if mag_index: #震级，默认是0-10
+    mag  = in_put[mag_index+1].split(',')
+    mag_min,mag_max = float(mag[0]),float(mag[1])
+    print (mag_min,mag_max)
+else:
+    mag_min,mag_max=0,10
+if sta_index:
+    sta_list = in_put[sta_index+1].split(',')
+    print (sta_list)
+if phase_index:
+    phase_list = in_put[phase_index+1].split(',')
+    print (phase_list)
+print (mag_max)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
